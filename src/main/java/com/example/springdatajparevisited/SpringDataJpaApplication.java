de233abeb3021b081c3e65b9e6e47804f2220566
amit7321 <amit.gunner73@gmail.com>
2024-03-24 00:01:25 +0600

    implement update and insert method

--- src/main/java/com/example/springdatajparevisited/SpringDataJpaApplication.java
+++ src/main/java/com/example/springdatajparevisited/SpringDataJpaApplication.java
@@ -1,12 +1,15 @@
 package com.example.springdatajparevisited;
 
 import com.example.springdatajparevisited.jpa.PersonJpaRepository;
+import com.example.springdatajparevisited.jpa.entity.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringDataJpaApplication implements CommandLineRunner {
@@ -22,7 +25,8 @@ public class SpringDataJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("All users -> {}", personJpaRepository.findById(1001));
+		logger.info("Users -> {}", personJpaRepository.insert(new Person("amit", "Dhaka", new Date())));
+		logger.info("All users -> {}", personJpaRepository.update(new Person("amit", "rangpur", new Date())));
 	}
 
 }
--- src/main/java/com/example/springdatajparevisited/jpa/PersonJpaRepository.java
+++ src/main/java/com/example/springdatajparevisited/jpa/PersonJpaRepository.java
@@ -1,10 +1,7 @@
 package com.example.springdatajparevisited.jpa;
 
-
-
 import com.example.springdatajparevisited.jpa.entity.Person;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Id;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
@@ -20,4 +17,18 @@ public class PersonJpaRepository {
         Person person = entityManager.find(Person.class, id);
         return person;
     }
+
+    public Person update(Person person) {
+         return entityManager.merge(person);
+    }
+
+    public Person insert(Person person) {
+        return entityManager.merge(person);
+    }
+
+    public void delete(int id) {
+        Person person =  findById(id);
+        entityManager.remove(person);
+    }
+
 }
