54301c7f88641c0a3b057d5f03876b6f31a65b3e
amit7321 <amit.hasan7321@gmail.com>
2024-09-24 20:59:12 +0600

    Implement native query.

--- src/test/java/com/example/springdatajparevisited/SpringDataJpaRevisitedApplicationTests.java
+++ src/test/java/com/example/springdatajparevisited/SpringDataJpaRevisitedApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.springdatajparevisited;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
 @SpringBootTest
 class SpringDataJpaRevisitedApplicationTests {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	EntityManager entityManager;
+
 	@Test
-	void contextLoads() {
+	public void native_query_basics() {
+		Query query = entityManager.createNativeQuery("select * from course where id = :id");
+		query.setParameter("id", 500L);
+		List resultList = query.getResultList();
+		logger.info("query",resultList);
 	}
 
 }
