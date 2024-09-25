a79af7cf6244f1970fc17e76ea02d289057478ac
amit7321 <amit.hasan7321@gmail.com>
2024-09-25 20:46:35 +0600

    implement student entity.

---
+++ src/main/java/com/example/springdatajparevisited/jpa/entity/Student.java
@@ -0,0 +1,41 @@
+package com.example.springdatajparevisited.jpa.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+
+}
